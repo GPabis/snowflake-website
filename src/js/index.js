9688365a1f7e26d3fc5b01a0fdb3afd4de92193e
GPabis <grzegorz.pabis.96@gmail.com>
2021-03-03 23:50:08 +0100

    Added contact-us section

--- src/js/index.js
+++ src/js/index.js
@@ -12,5 +12,28 @@ $('.gallery__slider').slick({
   slidesToScroll: 1,
   prevArrow: $('.gallery_arrow--l'),
   nextArrow: $('.gallery_arrow--r'),
-});
+  responsive: [
+    {
+      breakpoint: 996,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+  ]});
 
+function resizeInput(){
+  document.querySelectorAll(".form__input-box").forEach((elem)=>{
+    elem.addEventListener('input', (event) =>{
+      if(event.currentTarget.value){
+        elem.previousElementSibling.dataset.resized = true;
+      }
+      if(!event.currentTarget.value){
+        elem.previousElementSibling.dataset.resized = false;
+      }
+    })
+  })
+}
+
+resizeInput();
